dd5c9c15292250f3a6cfa16c4b715a645c0bbdc5
carnage67694325 <kingabdoeid2014@gmail.com>
2024-09-19 13:09:36 +0300

    created home view and adaptvie layout widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/features/home/presentation/view/home_view.dart';
 
 void main() {
   runApp(const ResponsiveDashBoardApp());
@@ -9,6 +10,8 @@ class ResponsiveDashBoardApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: HomeView(),
+    );
   }
 }
