2ea3947d7e0f1d69f5d80a0a24029b2589147e1f
carnage67694325 <kingabdoeid2014@gmail.com>
2024-09-19 12:59:46 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ResponsiveDashBoardApp());
+}
+
+class ResponsiveDashBoardApp extends StatelessWidget {
+  const ResponsiveDashBoardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
