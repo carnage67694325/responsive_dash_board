fa16fd8c541ff093c6322f724752bf9460f08cce
carnage67694325 <kingabdoeid2014@gmail.com>
2024-10-05 14:26:55 +0300

    added device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/features/home/presentation/view/home_view.dart';
 
 void main() {
-  runApp(const ResponsiveDashBoardApp());
+  runApp(DevicePreview(
+      enabled: true, builder: (context) => ResponsiveDashBoardApp()));
 }
 
 class ResponsiveDashBoardApp extends StatelessWidget {
@@ -10,9 +12,11 @@ class ResponsiveDashBoardApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      home: HomeView(),
+      home: const HomeView(),
     );
   }
 }
